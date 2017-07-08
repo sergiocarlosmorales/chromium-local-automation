8c7cf1b27b3ace0ea14b0a51a08f61c8d3c3ca4d
Sergio Carlos Morales Angeles <carloschilazo@gmail.com>
2017-07-08 14:30:20 -0500

    Support browser tests

--- buildAndRunChromium.sh
+++ buildAndRunChromium.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 FOLDER=$1
 COMPONENT=$2
+COMPONENT_ARGUMENTS=$3
 EXECUTABLE=""
 
 case "$COMPONENT" in
-chrome) EXECUTABLE='Chromium.app/Contents/MacOS/Chromium';;
+  chrome)
+    EXECUTABLE='Chromium.app/Contents/MacOS/Chromium'
+  ;;
+  browser_tests|browsertests)
+    # Define to support running with mis-typed component.
+    COMPONENT='browser_tests'
+    EXECUTABLE="$COMPONENT --gtest_filter=*$COMPONENT_ARGUMENTS*"
+  ;;
 esac
 
 ninja -C out/$FOLDER $COMPONENT && out/$FOLDER/$EXECUTABLE
